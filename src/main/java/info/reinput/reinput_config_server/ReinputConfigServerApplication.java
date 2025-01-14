34d58f062edd3abd72f8ab1dc139ebf4d9a3a6b3
MinhoJJang <ahchjang@naver.com>
2025-01-14 17:04:16 +0900

    Initial project setup with Gradle, Spring Boot, and basic application structure. Added .gitignore and .gitattributes files, along with Gradle wrapper scripts. Created main application class and test class for the Spring Boot application.

---
+++ src/main/java/info/reinput/reinput_config_server/ReinputConfigServerApplication.java
@@ -0,0 +1,13 @@
+package info.reinput.reinput_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinputConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/reinput/reinput_config_server/ReinputConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package info.reinput.reinput_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
